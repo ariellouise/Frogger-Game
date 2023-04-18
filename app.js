986b6448fa1d8d8bdb4f547c37670a32b960690d
ariellouise <arielladders@gmail.com>
2023-04-18 08:38:37 -0400

    updated app.jss
    added all const and began for move frog function

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const timeLeftDisplay = document.querySelector("#time-left");
-
 const resultDisplay = document.querySelector("#result");
-
 const startPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+const logsLeft = document.querySelectorAll(".log-left");
+const logsRight = document.querySelectorAll(".log-right");
+const carsLeft = document.querySelectorAll(".car-left");
+const carsRight = document.querySelectorAll(".car-right");
 
-const squares = document.querySelectorAll(".grid");
+let currentIndex = 76;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case "ArrowUp":
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case "ArrowDown":
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
+}
+
+document.addEventListener("keyup", moveFrog); /*callback function of moveFrog*/
