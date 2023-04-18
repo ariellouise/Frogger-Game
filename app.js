34e669f7d21191720193c30d7b871f7be4b1a7e9
ariellouise <arielladders@gmail.com>
2023-04-18 09:27:24 -0400

    updated app.js
    car movement updated

--- app.js
+++ app.js
@@ -87,16 +87,42 @@ function moveLogRight(logRight) {
 
 function moveCarLeft(carLeft) {
   switch (true) {
+    case carLeft.classList.contains('c1'):
+        carLeft.classList.remove('c1')
+        carLeft.classList.add('c2')
+        break
+    case carLeft.classList.contains('c2'):
+        carLeft.classList.remove('c2')
+        carLeft.classList.add('c3')
+        break
+    case carLeft.classList.contains('c3'):
+        carLeft.classList.remove('c3')
+        carLeft.classList.add('c1')
+        break
   }
 }
 
 function moveCarRight(carRight) {
   switch (true) {
+    case carRight.classList.contains('c1'):
+        carRight.classList.remove('c1')
+        carRight.classList.add('c3')
+        break
+    case carLeft.classList.contains('c2'):
+        carLeft.classList.remove('c2')
+        carLeft.classList.add('c1')
+        break
+    case carLeft.classList.contains('c3'):
+        carLeft.classList.remove('c3')
+        carLeft.classList.add('c2')
+        break
   }
 }
 
 startPauseButton.addEventListener('click', () => {
     if () {
 
-    }else {}
+    } else {
+        
+    }
 })
