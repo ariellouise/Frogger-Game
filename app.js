7229449068d032e0340055bb77a78eafd1c02fc9
ariellouise <arielladders@gmail.com>
2023-04-18 09:04:07 -0400

    app.js updated
    cases regarding the log movement for the left

--- app.js
+++ app.js
@@ -30,3 +30,32 @@ function moveFrog(e) {
 }
 
 document.addEventListener("keyup", moveFrog); /*callback function of moveFrog*/
+
+function autoMoveLogs() {
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+}
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains("l1"):
+      logLeft.clasList.remove("l1");
+      logLeft.classList.add("l2");
+      break;
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
+      break;
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
+  }
+}
